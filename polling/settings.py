5e63555e14252ab5227fc3f44def2899cd46b307
harishbisht29 <harishbisht29@gmail.com>
2019-09-11 02:26:08 +0530

    adding base.html template file for basic header and fotter

--- polling/settings.py
+++ polling/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'polling.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['polling/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
