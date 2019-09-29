82ba36b352b56618081793c347960fbf2854d3f3
harishbisht29 <harishbisht29@gmail.com>
2019-09-29 20:53:00 +0530

    adding show functionality

--- survey/urls.py
+++ survey/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('pick_survey_template/',views.template_picker,name="pick_survey_template"),
     path('save_survey_data/<int:template_id>',views.save_survey_data, name="save_survey_data"),
     path('delete_question_id/)',views.delete_question,name='delete_question_id'),
+    path('show_survey_page/)',views.show_survey_page,name='show_survey_page'),
    # path('accounts/', include('accounts.url')),
 ]
--- survey/views.py
+++ survey/views.py
@@ -27,7 +27,8 @@ def home_page(request):
     # print(Templates)
     Question_Details = []
     for template in Templates:
-        
+        template_id = template.id
+
         if template.name.upper() == "QUESTION_SURVEY":
             Questions = Question_Survey.objects.filter(creator=request.user.id).order_by('date_created')
             for question in Questions:
@@ -37,8 +38,9 @@ def home_page(request):
                 detail["question"] = question.question
                 detail["date_created"] = question.date_created_pretty()
                 detail["id"] = question.id
+                detail["template_id"] = template_id
                 Question_Details.append(detail)
-            
+        
 
     print(Question_Details)
     return(render(request,'survey/home_page.html',{"details":Question_Details}))
@@ -54,7 +56,7 @@ def save_survey_data(request, template_id):
     
     template = getSurveyTemplate(template_id)
     template_name = template.name.upper()
-
+    print("Saving survey Data")
     if request.method == "POST":
         if template_name == "QUESTION_SURVEY":
             print("Inside Save Survey Data")
@@ -89,3 +91,29 @@ def delete_question(request):
     # html = "<html><body>Delete Questid Page</body></html>"
     # return HttpResponse(html)
     return redirect('home_page')
+
+def show_survey_page(request):
+    question_id = request.POST["quest_id"]
+    survey_type = request.POST["survey_type"].upper()
+    template_id = request.POST["template_id"]
+
+    survey_template = Survey_Templates.objects.filter(id=template_id)[0]
+    output_tempate = survey_template.output_template
+    
+    print("Output Template is --------------"+ output_tempate)
+    data = {}
+    if survey_type == "QUESTION_SURVEY":
+        
+        print(question_id+" "+survey_type)
+        question = Question_Survey.objects.filter(id=question_id)[0]
+        print(question)
+    
+        html = "<html><body>Showing Survey Page"+ question.question+ "</body></html>"
+        data["question"] = question.question
+        data["option1"] = question.option1
+        data["option2"] = question.option2
+        data["option3"] = question.option3
+        data["option4"] = question.option4
+    print(data)
+    # return HttpResponse(html)
+    return(render(request,'survey/Output_Question_Survey.html',{"data":data}))
